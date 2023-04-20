d7c2617b23bebb65c6ad9b00627dd2adb35d1edd
bluezelli <96504071+bluezelli@users.noreply.github.com>
2023-04-20 14:16:22 +0200

    update gemaakt

--- src/Controller/AutoController.php
+++ src/Controller/AutoController.php
@@ -3,8 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Autos;
-use App\Repository\AutosRepository;
-use Doctrine\ORM\EntityManager;
+//use App\Repository\AutosRepository;
+//use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,11 +13,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class AutoController extends AbstractController
 {
     #[Route('/auto', name: 'app_auto')]
-    public function index(EntityManager $entityManager): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
         $autos = $entityManager->getRepository(Autos::class)->findAll();
         return $this->render('auto/index.html.twig', [
             "autos"=>$autos
         ]);
     }
+    #[Route('/auto/{id}', name: 'app_auto')]
+    public function details(EntityManagerInterface $entityManager, Autos $autoclass): Response
+    {
+        $id = $autoclass->getId();
+        $autos = $entityManager->getRepository(Autos::class)->find($id);
+        return $this->render('auto/details.html.twig', [
+            "autos"=>$autos
+            ]);
+    }
+
 }
